7cac98cc6c195d1202aefb67f67c9aebafedc133
Shriyans <shriyansbhatnagar@gmail.com>
2016-12-02 17:00:49 +0900

    added support for hot module reloading
    added support for hot module reloading using babel-hmre

--- packages/react-scripts/config/webpack.config.dev.js
+++ packages/react-scripts/config/webpack.config.dev.js
@@ -148,7 +148,7 @@ module.exports = {
         query: {
           // @remove-on-eject-begin
           babelrc: (paths.customBabelrc ? true : false),
-          presets: [require.resolve('babel-preset-react-app')],
+          presets: [require.resolve('babel-preset-react-app'), "react-hmre"],
           // @remove-on-eject-end
           // This is a feature of `babel-loader` for webpack (not Babel itself).
           // It enables caching results in ./node_modules/.cache/babel-loader/
